4a3422172ecb171c60c5c9b0dbb0e81091c7b583
nahuelsoaje <soajenahuel9@gmail.com>
2024-05-08 20:36:56 -0300

    creacion de Navbar

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src="" alt="Logo" />
+        <div>
+        <button>Iniciar Sesion</button>
+        <button>Crear Cuenta</button>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
