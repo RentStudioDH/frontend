a27d03f1fdbd095f21ee50322d1cd05dbcd28374
Facundo1414 <facu.allende14gmail.com>
2024-05-13 22:50:35 -0300

    se cambio las imagenes para la seccion de beneficios como tambien el color de fondo del mismo y de la pagina home

--- src/components/molecules/BeneficiosSeccion.jsx
+++ src/components/molecules/BeneficiosSeccion.jsx
@@ -6,9 +6,9 @@ import { Grid, Box } from '@mui/material';
 const BeneficiosSeccion = () => {
     return (
         <Box >
-            <Grid container spacing={2}>
+            <Grid container spacing={2} display={'flex'}py={"1rem"}>
                 {beneficiosData.map((card) => (
-                    <Grid key={card.id} item xs={6} md={3} >
+                    <Grid key={card.id} item xs={6} md={3}>
                         <CardBeneficios key={card.id} title={card.title} img={card.img} textInfo={card.textInfo}/>
                     </Grid>
                 ))}
--- src/components/organisms/HomeBodyRecomendados.jsx
+++ src/components/organisms/HomeBodyRecomendados.jsx
@@ -5,7 +5,7 @@ import BodyRecomendadosContainer from '../molecules/BodyRecomendadosContainer'
 
 const HomeBodyRecomendados = () => {
   return (
-    <Box width={"90%"} px={'5%'}> {/* Se puede modificar */}
+    <Box width={"80%"} mx={"10%"} padding={"1rem"} bgcolor={"#FFFFFF"}> {/* Se puede modificar */}
         <Typography variant='h5' fontWeight={'700'} color={'#56494E'} sx={{marginBottom: "10px",borderBottom: '4px solid #A62639', width: '20%'}}>Productos recomendados</Typography>
         <BodyRecomendadosContainer/>
     </Box>
