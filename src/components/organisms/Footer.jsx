39fe4898c1f2d1fbe29231bb678baf353579156e
JhonatanRios <jhoto1234@gmail.com>
2024-05-15 00:24:49 -0500

    terminar de ajustar footer responsive + criterios

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -5,7 +5,10 @@ const Footer = () => {
   return (
     <Box component="footer" className='d-grid' sx={{ placeItems: 'center' }}>
       <Box className='d-flex g-15' sx={{ width: 'min(1280px, 100%)', flexDirection: { xs: 'column', md: 'row' }, justifyContent: 'space-between', alignItems: 'center', padding: '15px' }}>
-        <Typography variant='body1' className='txt-center' sx={{ color: '#fff'}}>Todos los derechos reservados. Hecho por el <strong>Grupo7</strong></Typography>
+        <Box className='d-flex' sx={{ flexDirection: { xs: 'column', md: 'row' }, alignItems: 'center', gap: '10px'}}>
+          <img src="/img/logo-min.png" alt={'RS'} title={'RS'} width={25} height={25}/>
+          <Typography variant='body1' className='txt-center' sx={{ color: '#fff', fontSize: '10px', lineHeight: '1' }}>2024 - Todos los derechos reservados. Hecho por el <strong>Grupo7</strong></Typography>
+        </Box>
         <Box className='d-flex social g-15'>
           <Link><i className="fa-brands fa-square-facebook"></i></Link>
           <Link><i className="fa-brands fa-instagram"></i></Link>
