9179a3797e4fef3daac66f0e76b6aecbc0086e5f
Facundo1414 <facu.allende14gmail.com>
2024-05-15 21:52:44 -0300

    search input terminado

--- src/components/molecules/SeachInput.jsx
+++ src/components/molecules/SeachInput.jsx
@@ -1,23 +1,19 @@
-import * as React from 'react';
-import TextField from '@mui/material/TextField';
-import Stack from '@mui/material/Stack';
-import Autocomplete from '@mui/material/Autocomplete';
-import { Box, IconButton, InputBase, Paper } from '@mui/material';
+import React from 'react';
+import { IconButton, InputBase, Paper } from '@mui/material';
+import SearchIcon from '../../../public/searchIcon.png'; // Importa la imagen directamente
 
-export default function SearchInput() {
+export default function SearchInput({ searchHandler }) { // Desestructura las props
   return (
-    <Paper elevation={2} sx={{ width: 350, borderRadius: "15px"}}>
-        <InputBase  
-        sx={{ ml: 1, flex: 1 }}
-        placeholder="Search Google Maps"
+    <Paper elevation={2} sx={{ width: 350, borderRadius: "28px", display: 'flex', alignItems: 'center', justifyContent: 'space-between' }}>
+      <InputBase  
+        sx={{ ml: 2, flex: 1 }}
+        placeholder="Buscar cámaras..."
         color='#56494E'
-        inputProps={{ 'aria-label': 'search google maps' }}
+        inputProps={{ 'aria-label': 'Buscar cámaras...' }}
       />
-        <IconButton type="button" sx={{ p: '10px' }} aria-label="search"> 
-            <img src="../../../public/search_icon.png" alt="" />
-        </IconButton> 
-
+      <IconButton type="button" onClick={searchHandler} sx={{ p: '10px' }} aria-label="search"> 
+        <img width={"35px"} height={"35px"} src={SearchIcon} alt="search" /> {/* Usa la imagen importada */}
+      </IconButton> 
     </Paper>
   );
 }
-
