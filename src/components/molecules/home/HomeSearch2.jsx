73784c7c412161050eb64f4e1bff7e702501dd05
Facundo1414 <facu.allende14gmail.com>
2024-06-10 16:27:14 -0300

    se agrego un evento para detectar cuando es mobile .

--- src/components/molecules/home/HomeSearch2.jsx
+++ src/components/molecules/home/HomeSearch2.jsx
@@ -10,7 +10,8 @@ const HomeSearch2 = ({ title }) => {
   const [dates, setDates] = useState({ startDate: null, endDate: null })
   const [searchText, setSearchText] = useState('')
   const [buttonText, setButtonText] = useState('')
-  const [isFocused, setIsFocused] = useState(false) // Estado para manejar el foco
+  const [isFocused, setIsFocused] = useState(false)
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768)
 
   const handleSearch = (e) => {
     e.preventDefault()
@@ -22,6 +23,18 @@ const HomeSearch2 = ({ title }) => {
   }
 
   useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768)
+    }
+
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  useEffect(() => {
     if (selectedCategory || dates.startDate || dates.endDate || searchText) {
       setButtonText('Buscar')
     } else {
@@ -30,11 +43,15 @@ const HomeSearch2 = ({ title }) => {
   }, [selectedCategory, dates, searchText])
 
   const handleFocus = () => {
-    setIsFocused(true)
+    if (isMobile) {
+      setIsFocused(true)
+    }
   }
 
   const handleBlur = () => {
-    setIsFocused(false)
+    if (isMobile) {
+      setIsFocused(false)
+    }
   }
 
   return (
@@ -42,22 +59,22 @@ const HomeSearch2 = ({ title }) => {
       <h2 className='txt-primary text-center bigtitle'><strong>{title}</strong></h2>
       <form 
         onSubmit={handleSearch} 
-        className="flex bg-white search p-search g-5" 
+        className="flex bg-white search p-search g-5"
         onFocus={handleFocus} 
         style={{
-          flexDirection: isFocused ? 'column' : 'row',
-          alignItems: isFocused ? 'stretch' : 'center',
-          justifyContent: isFocused ? 'flex-start' : 'space-between'
+          flexDirection: isMobile && isFocused ? 'column' : 'row',
+          alignItems: isMobile && isFocused ? 'stretch' : 'center',
+          justifyContent: isMobile && isFocused ? 'flex-start' : 'space-between'
         }}
       >
-        <div style={{ marginBottom: isFocused ? '15px' : '0' }}>
-          <SearchText onSearchTextChange={setSearchText} />
+        <div style={{ marginBottom: isMobile && isFocused ? '15px' : '0' }}>
+          <SearchText onSearchTextChange={setSearchText} onFocus={handleFocus} onBlur={handleBlur} />
         </div>
-        <div style={{ marginBottom: isFocused ? '15px' : '0', display: isFocused ? "block" : 'none' }}>
-          <SearchDate onDatesChange={setDates} />
+        <div style={{ marginBottom: isMobile && isFocused ? '15px' : '0', display: isMobile && isFocused ? 'block' : isMobile ? 'none' : 'block' }}>
+          <SearchDate onDatesChange={setDates} onFocus={handleFocus} onBlur={handleBlur} />
         </div>
-        <div style={{ marginBottom: isFocused ? '15px' : '0', display: isFocused ? "block" : 'none' }}>
-          <SearchCategory onSelectCategory={setSelectedCategory} data={state} />
+        <div style={{ marginBottom: isMobile && isFocused ? '15px' : '0', display: isMobile && isFocused ? 'block' : isMobile ? 'none' : 'block' }}>
+          <SearchCategory onSelectCategory={setSelectedCategory} data={state} onFocus={handleFocus} onBlur={handleBlur} />
         </div>
         <button onClick={handleBlur} type="submit" className="p-2 bg-primary text-white rounded-full">
           <i className="fa-solid fa-magnifying-glass"></i> {buttonText}
