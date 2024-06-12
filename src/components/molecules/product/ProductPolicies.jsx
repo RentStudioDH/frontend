142e200051990522ff2b44ded11ca3a5042f51a2
aldrinj0se <aldrinsilvam@gmail.com>
2024-06-12 03:57:28 -0400

    Sección políticas definitiva

--- src/components/molecules/product/ProductPolicies.jsx
+++ src/components/molecules/product/ProductPolicies.jsx
@@ -1,46 +1,38 @@
 import { useEffect, useState } from "react";
-import { fetchData } from "../../../utils/js/apiRequest";
-import { useContextGlobal } from "../../../contexts/global.context";
 import { useParams } from "react-router-dom";
+import { useContextGlobal } from "../../../contexts/global.context";
 
 const ProductPolicies = ({ data }) => {
   const { id } = useParams();
   const { getProductById } = useContextGlobal();
-  const [policies, setPolicies] = useState([]);
-  const [error, setError] = useState(null);
+  const [showPolicies, setShowPolicies] = useState(false);
 
   useEffect(() => {
-    const fetchPolicies = async () => {
-      try {
-        const policiesData = await fetchData({
-          method: "get",
-          endpoint: `/public/products/policies`,
-          requireAuth: false,
-        });
-        setPolicies(policiesData);
-      } catch (error) {
-        setError(new Error("No se pudo obtener las políticas del producto."));
-      }
-    };
-    fetchPolicies();
+    if (data.policies.length !== 0) {
+      setShowPolicies(true);
+    }
   }, [id, getProductById]);
 
   return (
-    <div className="bg-white shadow-lg grid br-15 p-15 g-15">
-      <h2 className="txt-accent bb-primary subtitle">
-        <strong> Reglamentos de producto:</strong>
-      </h2>
-      <div className="flex">
-        {policies.map((policy) => (
-          <div key={policy.id} className=" p-15">
-            <p className="txt-accent paragraph">
-              <strong>{policy.title}</strong>
-            </p>
-            <p className="txt-tertiary paragraph">{policy.description}</p>
+    <>
+      {showPolicies && (
+        <div className="bg-white shadow-lg grid br-15 p-15 g-15">
+          <h2 className="txt-accent bb-primary subtitle">
+            <strong> Reglamentos de producto:</strong>
+          </h2>
+          <div className="grid grid-flow-row md:grid-flow-col gap-8">
+            {data.policies.map((policy) => (
+              <div key={policy.id} className="m-8">
+                <p className="flex items-start txt-accent paragraph pb-8">
+                  <strong>{policy.title}</strong>
+                </p>
+                <p className="txt-tertiary paragraph">{policy.description}</p>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
+        </div>
+      )}
+    </>
   );
 };
 
