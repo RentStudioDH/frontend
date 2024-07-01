5d6c7445ccb4dbef1f9f693493777ea87fed5d87
JhonatanRios <jhoto1234@gmail.com>
2024-07-01 17:26:44 -0500

    👌🏼Ajustar boton de fav y subir compartir

--- src/components/molecules/product/ProductHeader.jsx
+++ src/components/molecules/product/ProductHeader.jsx
@@ -1,32 +1,23 @@
-import { Link, useNavigate } from 'react-router-dom';
-import { useContextGlobal } from '../../../contexts/global.context';
-import Swal from 'sweetalert2';
+import { Link, useNavigate } from 'react-router-dom'
+import { useContextGlobal } from '../../../contexts/global.context'
+import Swal from 'sweetalert2'
+import ShareButton from '../share/ShareButton'
 
 const ProductHeader = ({ data }) => {
-  const navigate = useNavigate();
-  const { state, dispatch } = useContextGlobal();
+  const navigate = useNavigate()
+  const { state, toggleFav } = useContextGlobal()
+  const { favs } = state
+  const isFav = favs.includes(data.id)
 
-  const isFavorite = state.favs.some(fav => fav.id === data.id);
-
-  const handleToggleFav = (item) => {
-    if (isFavorite) {
-      dispatch({ type: 'REMOVE_FAV', payload: item.id });
-      Swal.fire({
-        icon: 'success',
-        title: 'Eliminado de favoritos',
-        showConfirmButton: false,
-        timer: 1500
-      });
-    } else {
-      dispatch({ type: 'ADD_FAV', payload: item });
-      Swal.fire({
-        icon: 'success',
-        title: 'Añadido a favoritos',
-        showConfirmButton: false,
-        timer: 1500
-      });
-    }
-  };
+  const handleToggleFav = () => {
+    toggleFav(data.id)
+    Swal.fire({
+      icon: 'success',
+      title: isFav ? 'Eliminado de favoritos' : 'Añadido a favoritos',
+      showConfirmButton: false,
+      timer: 1500
+    })
+  }
 
   return (
     <>
@@ -36,18 +27,12 @@ const ProductHeader = ({ data }) => {
         </Link>
         <h1 className='txt-primary title bb-primary'><strong>{data.name}</strong></h1>
       </div>
-      <button onClick={() => handleToggleFav(data)} className="flex items-center">
-        <i
-          className={`fa-solid fa-heart title ${isFavorite ? 'text-red-800 border-none ' : 'text-white  bg-slate-600'}`}
-          style={{
-            borderWidth: '2px',
-            padding: '0.35rem',
-            borderRadius: '50%',
-          }}
-        ></i>      
-      </button>
+      <div className='flex items-center g-15'>
+        <ShareButton product={data} />
+        <i onClick={() => handleToggleFav()} className={`fa-${isFav ? 'solid' : 'regular'} fa-heart cursor-pointer txt-primary hover:txt-accent title`}></i>
+      </div>
     </>
-  );
-};
+  )
+}
 
-export default ProductHeader;
+export default ProductHeader
