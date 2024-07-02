68d8ee216c2434ab888ec0ce097a3310b783b881
JhonatanRios <jhoto1234@gmail.com>
2024-07-01 19:55:10 -0500

    Acomodar politicas de producto

--- src/components/molecules/product/ProductPolicies.jsx
+++ src/components/molecules/product/ProductPolicies.jsx
@@ -2,18 +2,49 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { useContextGlobal } from "../../../contexts/global.context";
 
-const ProductPolicies = ({ data }) => {
+const ProductPolicies = () => {
   const { id } = useParams();
-  const { getProductById } = useContextGlobal();
+  const { state } = useContextGlobal();
   const [showPolicies, setShowPolicies] = useState(false);
-  const [policies, setPolicies] = useState(data.category.policies || []);
+  const [policies, setPolicies] = useState([]);
 
   useEffect(() => {
-    if (policies.length !== 0) {
-      setShowPolicies(true);
-    }
-  }, [id, getProductById]);
+    if (!showPolicies) {
+      const loadPolicies = () => {
+        const productId = parseInt(id);
+        // Encuentra el producto en el estado global
+        const product = state.data.find(product => product.id === productId);
 
+        if (product) {
+          console.log('Product found:', product);
+
+          // Obtén la categoría del producto
+          const productCategoryName = product.category.name;
+          console.log('Product category:', productCategoryName);
+
+          // Encuentra la categoría en el estado global
+          const category = state.categories.find(category => category.name === productCategoryName);
+
+          if (category && category.policies) {
+            console.log('Category found:', category);
+            // Actualiza el estado con las políticas de la categoría
+            setPolicies(category.policies);
+            setShowPolicies(true);
+          } else {
+            console.log('Category not found or no policies:', category);
+            setShowPolicies(false);
+          }
+        } else {
+          console.log('Product not found with id:', productId);
+          setShowPolicies(false);
+        }
+      };
+
+      loadPolicies();
+    }
+  }, [id, state.data, state.categories, showPolicies]); // Solo se ejecuta una vez al montar el componente y cuando las dependencias cambian
+
+  // console.log('Policies:', policies);
   return (
     <>
       {showPolicies && (
