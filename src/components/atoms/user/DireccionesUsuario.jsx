b543de5e5da5feff7e33a0dbaede523c9108faa1
Facundo1414 <facu.allende14gmail.com>
2024-07-01 21:51:14 -0300

    spinner para la direccion del usuario

--- src/components/atoms/user/DireccionesUsuario.jsx
+++ src/components/atoms/user/DireccionesUsuario.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Grid, Paper, Typography } from '@mui/material';
+import { Button, Grid, Paper, Typography, CircularProgress } from '@mui/material';
 import { Home as HomeIcon } from '@mui/icons-material';
 import EditAddressModal from './EditAddressModal';
 import { useContextGlobal } from '../../../contexts/global.context';
@@ -8,12 +8,19 @@ const DireccionesUsuario = () => {
   const { state, getUserData } = useContextGlobal();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [addresses, setAddresses] = useState([]);
+  const [loading, setLoading] = useState(true); // Estado para controlar la carga de datos
 
   useEffect(() => {
     const fetchUserData = async () => {
-      const userData = await getUserData();
-      if (userData && userData.address) {
-        setAddresses([userData.address]);
+      try {
+        const userData = await getUserData();
+        if (userData && userData.address) {
+          setAddresses([userData.address]);
+        }
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      } finally {
+        setLoading(false); // Cambia el estado a false después de la carga (incluso si hubo error)
       }
     };
     fetchUserData();
@@ -28,8 +35,13 @@ const DireccionesUsuario = () => {
   };
 
   const handleSaveAddress = (newAddress) => {
-    setAddresses([newAddress]);
-    setIsModalOpen(false);
+    setLoading(true); // Activar el spinner al guardar la dirección
+    // Simular una operación de guardado (aquí deberías implementar la lógica real)
+    setTimeout(() => {
+      setAddresses([newAddress]);
+      setIsModalOpen(false);
+      setLoading(false); // Desactivar el spinner después de guardar
+    }, 1500); // Ejemplo de retardo para simular una operación de guardado
   };
 
   return (
@@ -38,51 +50,53 @@ const DireccionesUsuario = () => {
         <strong>Dirección</strong>
       </Typography>
       <Paper elevation={4} sx={{ padding: 2, borderRadius: "9px" }}>
-        <Grid container spacing={2} alignItems="center">
-          {addresses.length === 0 ? (
-            <Grid item xs={12} sx={{ textAlign: 'center', height: "10rem" }}>
-              <Typography variant="h6" color="#56494E">
-                No tienes ninguna dirección guardada.
-              </Typography>
-              <Button 
-                variant="contained" 
-                sx={{
-                  bgcolor: "#A62639",
-                  marginTop: 2,
-                  color: "white",
-                  '&:hover': {
-                    bgcolor: "#8C1D31", // Color de fondo al pasar el cursor sobre el botón
-                  },
-                }}
-                onClick={handleOpenModal}
-              >
-                Agregar Dirección
-              </Button>
-            </Grid>
-          ) : (
-            addresses.map((address, index) => (
-              <Grid item xs={12} key={index}>
-                <Paper elevation={2} sx={{ padding: 2, borderRadius: "9px" }}>
-                  <Grid container spacing={2} alignItems="center">
-                    <Grid item xs={1}><HomeIcon /></Grid>
-                    <Grid item xs={9}>
-                      <Typography variant='h4' color={"#56494E"} fontWeight={500}>{address.street}</Typography>
-                      <Typography variant='h6' fontWeight={400}>{address.city}, {address.state}, {address.country}</Typography>
-                      <Typography variant='h6' fontWeight={400}>{`${state.user.firstName} ${state.user.lastName}`}</Typography>
-                    </Grid>
-                    <Grid item xs={1}>
-                      <Button sx={{ color: "#A62639", '&:hover': { bgcolor: "#A62639", color: "white" } }} onClick={handleOpenModal}>
-                        Editar
-                      </Button>
-                    </Grid>
+        {loading ? (
+          <Grid container justifyContent="center" alignItems="center" sx={{ height: '10rem' }}>
+            <CircularProgress />
+          </Grid>
+        ) : addresses.length === 0 ? (
+          <Grid item xs={12} sx={{ textAlign: 'center', height: "10rem" }}>
+            <Typography variant="h6" color="#56494E">
+              No tienes ninguna dirección guardada.
+            </Typography>
+            <Button 
+              variant="contained" 
+              sx={{
+                bgcolor: "#A62639",
+                marginTop: 2,
+                color: "white",
+                '&:hover': {
+                  bgcolor: "#8C1D31", // Color de fondo al pasar el cursor sobre el botón
+                },
+              }}
+              onClick={handleOpenModal}
+            >
+              Agregar Dirección
+            </Button>
+          </Grid>
+        ) : (
+          addresses.map((address, index) => (
+            <Grid item xs={12} key={index}>
+              <Paper elevation={2} sx={{ padding: 2, borderRadius: "9px" }}>
+                <Grid container spacing={2} alignItems="center">
+                  <Grid item xs={1}><HomeIcon /></Grid>
+                  <Grid item xs={9}>
+                    <Typography variant='h4' color={"#56494E"} fontWeight={500}>{address.street}</Typography>
+                    <Typography variant='h6' fontWeight={400}>{address.city}, {address.state}, {address.country}</Typography>
+                    <Typography variant='h6' fontWeight={400}>{`${state.user.firstName} ${state.user.lastName}`}</Typography>
                   </Grid>
-                </Paper>
-              </Grid>
-            ))
-          )}
-        </Grid>
-        <EditAddressModal open={isModalOpen} handleClose={handleCloseModal} handleSave={handleSaveAddress} />
+                  <Grid item xs={1}>
+                    <Button sx={{ color: "#A62639", '&:hover': { bgcolor: "#A62639", color: "white" } }} onClick={handleOpenModal}>
+                      Editar
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Paper>
+            </Grid>
+          ))
+        )}
       </Paper>
+      <EditAddressModal open={isModalOpen} handleClose={handleCloseModal} handleSave={handleSaveAddress} />
     </>
   );
 };
