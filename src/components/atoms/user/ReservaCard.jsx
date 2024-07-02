1ddbabc03f3b293a9c38a9d3b872a59100beab39
Facundo1414 <facu.allende14gmail.com>
2024-07-01 21:08:06 -0300

    reserva funcional

--- src/components/atoms/user/ReservaCard.jsx
+++ src/components/atoms/user/ReservaCard.jsx
@@ -1,39 +1,63 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Paper, Typography, Box, Button } from '@mui/material';
+import { useContextGlobal } from '../../../contexts/global.context';
 
 const ReservaCard = ({ reservation, onClick }) => {
+  const { getProductById } = useContextGlobal();
+  const [productData, setProductData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getProductById(reservation.productId);
+        setProductData(data);
+      } catch (error) {
+        console.error('Error fetching product data:', error);
+      }
+    };
+
+    fetchData();
+  }, [reservation, getProductById]);
+
+  const firstImage =
+    productData?.attachments?.length > 0
+      ? productData.attachments[0].url
+      : 'https://digitalhouse-e7-pi.s3.amazonaws.com/-Rhd-l2yWTj6iEqg7EhN9Q%3D%3D.png';
+
   return (
     <Paper 
       elevation={3} 
-      sx={{ display: 'flex', marginBottom: 2, cursor: 'pointer' }}
+      sx={{ display: 'flex', alignItems: 'center', marginBottom: 2, cursor: 'pointer' }}
       onClick={() => onClick(reservation.id)}
     >
       <Box 
         component="img"
         sx={{ width: '40%', height: '100%', maxHeight: "200px" , objectFit: 'cover' }}
-        src={reservation.image}
-        alt={reservation.name}
+        src={firstImage}
+        alt={productData?.attachments?.[0]?.fileName || 'Default Image'}
       />
-      <Box sx={{ display: 'flex', flexDirection: 'column', padding: 2, width: '60%' }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', padding: 2, width: '50%' }}>
         <Typography variant="h4" component="div" color={"#511C29"}>
-          {reservation.name}
+          {productData?.name || 'Loading...'}
         </Typography>
         <Typography variant="body1" color="text.secondary" sx={{ marginTop: 1 }}>
           Fecha: {reservation.creationDateTime}
         </Typography>
       </Box>
       <Button 
-            variant="contained" 
-            sx={{ 
-              bgcolor: "#A62639", 
-              color: "white",
-              '&:hover': {
-                bgcolor: "#A62639" // Mantén el mismo color al hacer hover
-              } 
-            }} 
-          >
-            Volver a Reservar
-          </Button>
+        variant="contained" 
+        sx={{ 
+          bgcolor: "#A62639", 
+          color: "white",
+          alignSelf: 'flex-end',
+          margin: 2,
+          '&:hover': {
+            bgcolor: "#A62639" // Mantén el mismo color al hacer hover
+          } 
+        }} 
+      >
+        Volver a Reservar
+      </Button>
     </Paper>
   );
 };
